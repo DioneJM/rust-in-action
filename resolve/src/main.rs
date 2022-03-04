383dc1e9903bf1cafc96d554e42f5a501874adca
Dione <dionemorales@outlook.com>
2022-03-05 09:44:13 +1100

    fix bug where response was being saved in request buffer

--- resolve/src/main.rs
+++ resolve/src/main.rs
@@ -20,43 +20,42 @@ fn main() {
         .parse()
         .expect("Invalid Address");
 
-    let mut request_as_bytes: Vec<u8> = Vec::with_capacity(512);
-    let mut response_as_bytes: Vec<u8> = vec![0; 512];
+    println!("dns server: {:?}", dns_server);
 
-    let mut msg = Message::new();
-    msg
+    let mut request_buffer: Vec<u8> = Vec::with_capacity(512);
+    let mut response_buffer: Vec<u8> = vec![0; 512];
+
+    let mut request = Message::new();
+    request.add_query(Query::query(domain_name, RecordType::A));
+    request
         .set_id(rand::random::<u16>())
         .set_message_type(MessageType::Query)
-        .add_query(Query::query(domain_name, RecordType::A))
         .set_op_code(OpCode::Query)
         .set_recursion_desired(true);
-
-    let mut encoder = BinEncoder::new(&mut request_as_bytes);
-    msg.emit(&mut encoder).unwrap();
 
     let localhost = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind localhost");
     let timeout = Duration::from_secs(3);
     localhost.set_read_timeout(Some(timeout)).unwrap();
     localhost.set_nonblocking(false).unwrap();
 
-    let _amt = localhost.send_to(&request_as_bytes, dns_server)
+    let mut encoder = BinEncoder::new(&mut request_buffer);
+    request.emit(&mut encoder).unwrap();
+
+    let _amt = localhost.send_to(&request_buffer, dns_server)
         .expect("socket misconfigured");
 
     loop {
         let (_amt, remote_port) = localhost
-            .recv_from(&mut request_as_bytes)
+            .recv_from(&mut response_buffer)
             .expect("timeout reached");
 
         if remote_port == dns_server {
-            println!("breaking: {:?}", remote_port);
             break;
         }
     }
 
-    let dns_message = Message::from_vec(&response_as_bytes)
+    let dns_message = Message::from_vec(&response_buffer)
         .expect("Failed to parse response");
-
-    println!("answers: {:?}", dns_message.answers());
 
     for answer in dns_message.answers() {
         if answer.record_type() == RecordType::A {
